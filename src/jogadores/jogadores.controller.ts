d3061ce762f441882f81e75ad0414bced5b60701
Antonio Alves <antonio.jpsalves@gmail.com>
2024-07-27 15:25:20 -0300

    :sparkles: feat: rota para recuperar todos os jogadores em memoria

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { CriarJogadorDto } from './dtos/criar-jogador.dto'
 import { JogadoresService } from './jogadores.service'
 
@@ -10,5 +10,10 @@ export class JogadoresController {
   @Post()
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
     await this.jogadoresService.criarAtualizarJogador(criarJogadorDto)
+  }
+
+  @Get()
+  async consultarTodosJogadores() {
+    return await this.jogadoresService.consultarTodosJogadores()
   }
 }

20bbd38705f387d71142e30b5842c08fad1ecd85
Antonio Alves <antonio.jpsalves@gmail.com>
2024-07-27 14:08:04 -0300

    :tada: iniciando estudos em nestjs

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  @Post()
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { nome } = criarJogadorDto;
+
+    return JSON.stringify({
+      nome: `${nome}`,
+    });
+  }
+}
---
+++ src/jogadores/jogadores.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { JogadoresController } from './jogadores.controller';
+
+@Module({
+  controllers: [JogadoresController],
+})
+export class JogadoresModule {}
